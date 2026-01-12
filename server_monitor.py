87e12433562f4fadbda6ccd77ed0fe166b0056d5
dpacgdm <dpac.gdm@gmail.com>
2026-01-12 19:47:48 +0530

    Phase 4: Modified the script for writing the logs to system_health.log

--- server_monitor.py
+++ server_monitor.py
@@ -1,27 +1,35 @@
 import psutil
 import time
-from datetime import datetime
+import logging
+
+logging.basicConfig(
+    filename="system_health.log",
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
+
+console_handler=logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logging.getLogger().addHandler(console_handler)
 
 CPU_THRESHOLD = 80
 MEMORY_THRESHOLD = 80
 
-print("Starting system monitor...(Press Ctrl+C to stop)")
+print("Monitoring started...logs are being saved to system_health.log")
 
 try:
     while True:
         cpu_usage = psutil.cpu_percent(interval=1)
         memory_usage = psutil.virtual_memory().percent
         
-        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        print(f"CPU Usage: {cpu_usage}%")
-        print(f"Memory Usage: {memory_usage}%")
+        logging.info(f"CPU: {cpu_usage}% | Memory: {memory_usage}%")
         
         if cpu_usage > CPU_THRESHOLD or memory_usage > MEMORY_THRESHOLD:
             print("ALERT: High system usage detected!")
             
     
 except KeyboardInterrupt:
-    print("\nSystem monitor stopped.")
+    print("\nStopping")
 except Exception as e:
     print(f"An error occurred: {e}")
